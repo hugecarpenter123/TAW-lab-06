1a1ab01f83d26f2787defb36937be919f1a1408a
arkkupi85@gmail.com <arkkupi85@gmail.com>
2024-06-04 15:36:09 +0200

    add complete lab-09 angular app

--- api/lib/middlewares/loggerServer.middleware.ts
+++
@@ -1,6 +0,0 @@
-import { NextFunction, RequestHandler, Request, Response } from "express";
-
-export const loggerServer: RequestHandler = (request: Request, response: Response, next: NextFunction) => {
-    console.log(`${request.method} - ${request.url} - ${response.statusCode} - ${new Date().toISOString()}`);
-    next();
-}
